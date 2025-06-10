6fb608f3cc2e5c5f3a5844245e859d8b3f9d4934
Gabriela Ribeiro <gaabribeirosilva@gmail.com>
2025-06-10 09:18:43 -0300

    Realizei modificaçoes em cliente.py

--- historico.py
+++ historico.py
@@ -5,6 +5,21 @@ class Historico:
     def addTransacoes(self, transacao):
         self._transacoes.append(transacao)
 
+#  def adicionar_transacao(self, tipo, valor):
+#         data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+#         transacao = {
+#             "tipo": tipo,
+#             "valor": valor,
+#             "data": data_hora
+#         }
+#         self.__transacoes.append(transacao)
+
     def gerarExtrato(self):
         for transacao in self._transacoes:
             print(transacao)
+
+# def gerar_extrato(self, ultimas_n=10):
+#         print("\n--- EXTRATO ---")
+#         for transacao in self.__transacoes[-ultimas_n:]:
+#             print(f"{transacao['data']} - {transacao['tipo']}: R${transacao['valor']:.2f}")
+#         print("---------------")
